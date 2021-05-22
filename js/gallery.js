936202877f4149af623e0c0ce1ea1cd46ec162b9
xtayzx <taylen@shaw.ca>
2021-05-22 16:40:07 -0700

    moving listener
    after the function for the image to load has been loaded lol idk if this works

--- js/gallery.js
+++ js/gallery.js
@@ -147,8 +147,9 @@ if(galleryImages) {
 
             //we put the pause in the code to allow the buttons to be created and then after to reassign the opacity value
             //this way we are able to create the transition effect to load the gallery viewport components
-            image.addEventListener("click", loadButtons());
+           
         }
+        image.addEventListener("click", loadButtons());
     });
 }
 
