d56b0dbf3a54c2335f1f83a975423fb664d1ca38
xtayzx <taylen@shaw.ca>
2021-05-22 17:09:30 -0700

    move event listener 2
    i don't think this will run 42 times actually

--- js/gallery.js
+++ js/gallery.js
@@ -148,8 +148,9 @@ if(galleryImages) {
             //we put the pause in the code to allow the buttons to be created and then after to reassign the opacity value
             //this way we are able to create the transition effect to load the gallery viewport components
         }
-        document.querySelector(".img-btn-next").addEventListener("load", loadButtons());
+        
     });
+    document.querySelector(".img-btn-next").addEventListener("load", loadButtons());
 }
 
 function loadButtons() {
